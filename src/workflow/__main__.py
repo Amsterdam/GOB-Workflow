2f2d348724d21ebde0208216b2864b10f90e583f
vegter <amsjurien@gmail.com>
2018-07-16 12:25:40 +0200

    Proposal routing
    Resolves issue #5
    Route proposals by rerouting as request
    Proposals are always accepted

--- src/workflow/__main__.py
+++ src/workflow/__main__.py
@@ -4,7 +4,7 @@ from workflow.config import MESSAGE_BROKER, QUEUES, WORKFLOW_QUEUE, LOG_QUEUE
 from workflow.message_broker.async_message_broker import AsyncConnection
 
 
-def on_message(queue, key, msg):
+def on_message(connection, queue, key, msg):
     """Called on every message receipt
 
     :param queue: the message broker queue
@@ -12,18 +12,24 @@ def on_message(queue, key, msg):
     :param msg: the contents of the message
     :return:
     """
-    if queue == LOG_QUEUE:
+
+    if queue["name"] == LOG_QUEUE:
+        # For now, print the message to stdout
         # Eventually store the log message in a database
-        print("Log", msg)
-    elif queue == WORKFLOW_QUEUE:
-        # Eventually route message to next component
-        # fullimport.proposal => fullimport.request (compare against current data)
-        # fullupdate.proposal => fullupdate.request (update current data)
-        # fullupdate.completed (update process finished)
-        print("Workflow", msg)
+        print(msg["msg"])
+    elif queue["name"] == WORKFLOW_QUEUE:
+        if key == "fullimport.proposal":
+            print("=> 'fullimport.proposal' accepted, publish 'fullimport.request'")
+            connection.publish(queue, "fullimport.request", msg)
+        elif key == "fullupdate.proposal":
+            print("=> 'fullupdate.proposal' accepted, publish 'fullupdate.request'")
+            connection.publish(queue, "fullupdate.request", msg)
+        else:
+            print("Unknown workflow message received", key)
+            return False  # ignore message, leave for someone else
     else:
-        # This should never happen, the subscription routing_key should filter the messages
-        print("Unknown message type received")
+        # This should never happen...
+        print("Unknown message type received", queue["name"], key)
         return False  # Do not acknowledge the message
 
     return True  # Acknowledge message when it has been fully handled
@@ -35,6 +41,8 @@ with AsyncConnection(MESSAGE_BROKER) as connection:
     connection.subscribe(QUEUES, on_message)
 
     # Repeat forever
+    print("Workflow manager started")
     while True:
-        # Report some statistics or whatever is useful
         time.sleep(60)
+        # Report some statistics or whatever is useful
+        print(".")
--- src/workflow/config.py
+++ src/workflow/config.py
@@ -2,15 +2,17 @@ import os
 
 MESSAGE_BROKER = os.environ["MESSAGE_BROKER_ADDRESS"]
 
-WORKFLOW_QUEUE = "gob.workflow"
-LOG_QUEUE = "gob.log"
+WORKFLOW_QUEUE = "gob.workflow.proposal"
+LOG_QUEUE = "gob.log.all"
 
 QUEUES = [
     {
+        "exchange": "gob.workflow",
         "name": WORKFLOW_QUEUE,
-        "key": "#"
+        "key": "*.proposal"
     },
     {
+        "exchange": "gob.log",
         "name": LOG_QUEUE,
         "key": "#"
     }
