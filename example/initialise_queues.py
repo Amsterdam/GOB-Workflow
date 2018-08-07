791324403c05edfb473eb5ea33736c6c4b96f149
Marten Hoekstra <marten@dolhuis.nl>
2018-08-07 09:52:48 +0200

    Add code to run locally Fixes #7
    To be able to run locally this project was dependant on
    GOB-Message-Broker, whose only purpose was the provide functionality to
    run GOB-Workflow (and other components) locally.
    
    This commit adds code from that project to this one, so that it can be
    run locally without that project. The GOB-Message-Broker can be
    discontinued.

---
+++ example/initialise_queues.py
@@ -0,0 +1,60 @@
+import sys
+import pika
+
+
+def create_durable_message_queue(exchange, channel, name, route):
+    """Creates a persistent message queue on the RabbitMQ message broker
+
+    :param exchange: RabbitMQ exchande
+    :param channel: RabbitMQ channel
+    :param name: The name for queue to create
+    :param route: The route(s) on the queue
+    :return: RabbitMQ exchange
+    """
+
+    print(f"Create durable message queue {name} {route}")
+
+    channel.queue_declare(
+        queue=name,
+        durable=True
+    )
+
+    channel.queue_bind(
+        queue=name,
+        exchange=exchange,
+        routing_key=route
+    )
+
+
+if __name__ == "__main__":
+    try:
+        with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
+            channel = connection.channel()
+
+            WORKFLOW = "gob.workflow"
+            LOG = "gob.log"
+
+            for exchange in [WORKFLOW, LOG]:
+                channel.exchange_declare(
+                    exchange=exchange,
+                    exchange_type="topic",
+                    durable=True)
+
+            QUEUES = [
+                (WORKFLOW, WORKFLOW + ".proposal", "*.proposal"),
+                (WORKFLOW, WORKFLOW + ".request", "*.request"),
+                (LOG, LOG + ".all", "#"),
+            ]
+
+            for exchange, name, route in QUEUES:
+                create_durable_message_queue(
+                    exchange=exchange,
+                    channel=channel,
+                    name=name,
+                    route=route)
+
+            print(f"Succesfully created RabbitMQ message queues at '{RABBITMQ}'")
+
+    except Exception as e:
+        print(f"Error: Failed to connect to RabbitMQ at '{RABBITMQ}', {str(e)}")
+        sys.exit(1)
