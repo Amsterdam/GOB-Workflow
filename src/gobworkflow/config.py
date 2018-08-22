ed9add9743d418cdc6f17f274f9153bef9158ec9
Marten Hoekstra <marten@dolhuis.nl>
2018-08-22 14:05:32 +0200

    Add initialisation of queues to .jenkins
    The workflow service is in charge of the initialisation of queues. A
    local script was present, but not one the allow initialisation on
    release and deploy.
    
    That is now added.
    
    Creation of queues is idempotent, it will not destroy existing queues.

--- src/gobworkflow/config.py
+++ src/gobworkflow/config.py
@@ -12,18 +12,23 @@ import os
 
 MESSAGE_BROKER = os.environ["MESSAGE_BROKER_ADDRESS"]
 
-WORKFLOW_QUEUE = "gob.workflow.proposal"
-LOG_QUEUE = "gob.log.all"
+WORKFLOW_QUEUE = "gob.workflow"
+LOG_QUEUE = "gob.log"
 
 QUEUES = [
     {
         "exchange": "gob.workflow",
-        "name": WORKFLOW_QUEUE,
+        "name": WORKFLOW_QUEUE+'.proposal',
         "key": "*.proposal"
     },
     {
+        "exchange": "gob.workflow",
+        "name": WORKFLOW_QUEUE+'.request',
+        "key": "*.request"
+    },
+    {
         "exchange": "gob.log",
-        "name": LOG_QUEUE,
+        "name": LOG_QUEUE+'.all',
         "key": "#"
     }
 ]
